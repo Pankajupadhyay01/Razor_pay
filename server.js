5f53da825b2326564f8c9218cfb954e047b5723b
pankaj upadhyay <pankajup.git01@gmail.com>
2024-12-12 02:12:25 +0530

    vercel file updated

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ export const instance = new Razorpay({
     key_secret: process.env.RAZORPAY_APT_SECRET,
 });
 
+
 app.listen(process.env.PORT, () => {
     console.log("Server is running on port 4000")
 })
