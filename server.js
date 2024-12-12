19b532802a1153db7f25ea651c5855f160c43a75
pankaj upadhyay <pankajup.git01@gmail.com>
2024-12-12 11:08:16 +0530

    env removed

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ import { dbConn } from "./config/db/dbconn.js";
 
 dbConn()
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
-})
-
 export const instance = new Razorpay({
     key_id: process.env.RAZORPAY_API_KEY,
     key_secret: process.env.RAZORPAY_APT_SECRET,
 });
 
 
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
